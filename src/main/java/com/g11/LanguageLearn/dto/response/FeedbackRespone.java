ccd5364fe63119e0724623e6766c041120139748
minhninh <nguyenminhninh272002@gmail.com>
2024-04-08 08:40:58 +0700

    aka

---
+++ src/main/java/com/g11/LanguageLearn/dto/response/FeedbackRespone.java
@@ -0,0 +1,6 @@
+package com.g11.LanguageLearn.dto.response;
+
+public class FeedbackRespone { // doi tuong tra ve frontend
+    private String content;
+    private String imageFeedback;
+}
---
+++ src/main/java/com/g11/LanguageLearn/dto/response/UtilityResponse.java
@@ -0,0 +1,10 @@
+package com.g11.LanguageLearn.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UtilityResponse { //đối tượng trả ra front-end
+    private String name;
+
+
+}
