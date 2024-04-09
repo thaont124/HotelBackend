7c337968399a4e93b90289205cdd83d9ad5343da
LeThiQuynhNhi1801 <LeThiQuynhNhi1801@gmail.com>
2024-04-09 15:35:54 +0700

    update-myvoucher

--- src/main/java/com/g11/LanguageLearn/repository/ExchangeVoucherRepository.java
+++ src/main/java/com/g11/LanguageLearn/repository/ExchangeVoucherRepository.java
@@ -5,11 +5,13 @@ import com.g11.LanguageLearn.entity.ExchangeVoucher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Objects;
 
+@Repository
 public interface ExchangeVoucherRepository extends JpaRepository<ExchangeVoucher,Integer> {
-    @Query("select e.idExchangeVoucher,e.voucher.nameVoucher,e.voucher.pointVoucher,e.voucher.priceVoucher,e.statusVoucher from ExchangeVoucher e where e.user.idUser =:id order by ex.idExchangeVoucher DESC")
+    @Query("select e.idExchangeVoucher,e.voucher.nameVoucher,e.voucher.pointVoucher,e.voucher.priceVoucher,e.statusVoucher from ExchangeVoucher e where e.user.idUser =:id order by e.idExchangeVoucher DESC")
     List<MyVoucherResponse> getExchangeVouchersByIdUser(@Param("id") Integer id);
 }
